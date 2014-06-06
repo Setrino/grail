0be58e487fec9ea5e4bfc1a5614d04b58bc2a65c
Setrino <sk@fkdn-lab.com>
2014-06-06 10:36:31 +0200

    Update place.html

---
+++ js/place.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $('.right_slider, .left_slider').click(function(){
+        var slider = $('.slider');
+        if(slider.attr('src') == 'images/Screen%20Shot%202014-04-15%20at%2023.06.27.png'){
+            slider.attr('src', 'images/Screen%20Shot%202014-04-15%20at%2023.23.05.png');
+            $('.top_text').text('6.461231° 124.670698°');
+            $('.bottom_text').text('La Grail Srl, Via Gradisca, 66/1, Pasiano Pordenone');
+        }else{
+            slider.attr('src', 'images/Screen%20Shot%202014-04-15%20at%2023.06.27.png');
+            $('.top_text').text('26.983731° 4.637498°');
+            $('.bottom_text').text('Holy Grail Pharmacy, Santo Nino, Phillippines');
+        }
+    });
+});
